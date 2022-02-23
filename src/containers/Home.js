b31037c3f831c78e9d51470351c341e5ec72e81e
Masa Musa <masa@totheworld.com>
2022-02-23 10:52:44 -0500

    Start up launch

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -83,9 +83,9 @@ export default function Home() {
             {/* Jumbotron */}
             <div className="row d-flex justify-content-center align-content-center p-0" style={{ minHeight: "calc(100vh)", backgroundColor: "#ffffffeb" }}>
                 <div className="col-12 col-md d-flex justify-content-center align-content-center my-5 my-md-0">
-                    <div className="p-5 border rounded align-self-center">
+                    <div className="p-3 border rounded align-self-center">
                         {/* Background Video */}
-                        <video id="promote-video" loop autoPlay className="bg-white border border-secondary p-1 rounded shadow" style={{ width : "350px" }}>
+                        <video id="promote-video" loop autoPlay className="bg-white border border-secondary p-1 rounded shadow" style={{ width : "300px" }}>
                             <source src={promoteVideo} type="video/mp4" />
                             <source src={promoteVideo} type="video/ogg" />
                             Your browser does not support the video tag.
@@ -93,7 +93,7 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="col-12 col-md d-flex justify-content-center align-content-center">
-                    <div className="p-5 align-self-center">
+                    <div className="p-3 align-self-center">
                         <h1 style={{ fontSize: "2.5rem" }}> <b>SmAC</b> </h1>
                         <p style={{ fontSize: "1.2rem" }}> Incentive-driven solutions to help your business or project grow at the blockchain speed. Launch your smart ads in <span className="border-bottom">seconds</span>. </p>
                         <ul className="list-group list-group-flush">
